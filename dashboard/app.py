e01054999439568f68c1784bef5c3dab2c97a04d
Karla C. Ferreira <karlinharural@gmail.com>
2025-07-15 22:31:04 +0000

    fix

--- dashboard/app.py
+++ dashboard/app.py
@@ -26,7 +26,6 @@ opcao = st.sidebar.radio("O que deseja visualizar?", ["📊 Visão Geral", "📋
 # === FILTRO DE DADOS ===
 filtro = df[(df["estado"] == estado) & (df["ano"] == ano)]
 
-
 prod_medio = filtro["producao_litros"].mean()
 preco_medio = filtro["preco_litro"].mean()
 temp_media = filtro["temperatura_media"].mean()
